8015ed84c433711fd157ef47e4115fe20a471872
richardzhang121 <richard.zhang121@gmail.com>
2017-04-07 23:25:37 -0700

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -1,37 +1,33 @@
+//Node cpp methods initiated
 #include "Node.h"
 #include <iostream>
 
 using namespace std;
 
 Node* Node::getLeft(){
-    return left;
+  return left;
 }
-
 Node* Node::getRight(){
-    return right;
+  return right;
 }
-
 int Node::getData(){
-    return data;
+  return data;
 }
-
 void Node::setLeft(Node* node){
-    left = node;
+  left = node;
 }
-
 void Node::setRight(Node* node){
-    right = node;
+  right = node;
 }
-
 Node::Node(int number){
-    data = number;
+  left = NULL;
+  right = NULL;
+  data = number;
 }
-
 Node::Node(){
-    cout << "What do you want to input in your node?" << endl;
-    cin >> data;
+  cout << "What is your data for a new node" << endl;
+  cin >> data;
 }
-
 Node::~Node(){
-    delete &data;
+  delete &data;
 }
