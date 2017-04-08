c4285f6c25e58472eb690d0e9f60650d16ea6fa1
richardzhang121 <richard.zhang121@gmail.com>
2017-04-07 23:25:54 -0700

    Update Node.h

--- Node.h
+++ Node.h
@@ -1,17 +1,20 @@
+//Methods that will be used for binary search tree
+
 using namespace std;
 
+
 class Node{
-    public: 
-        Node(int newData);
-        Node();
-        ~Node();
-        Node* getLeft();
-        Node* getRight();
-        int getData();
-        void setLeft(Node* node);
-        void setRight(Node* node);
-    protected:
-        int data;
-        Node* left;
-        Node* right;
+ public:
+  Node(int newData);
+  Node();
+  ~Node();
+  Node* getLeft(); //returns the next node
+  Node* getRight();
+  int getData(); //returns the student the node points to
+  void setLeft(Node* node); //sets the pointer to the next node
+  void setRight(Node* node); 
+ protected:
+  int data;
+  Node* left;
+  Node* right;
 };
